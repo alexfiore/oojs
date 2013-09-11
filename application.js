eff431c00833f2a8fb26350dfdb88df3f5807562
Alex Fiore <writefiore@gmail.com>
2013-09-11 00:23:32 -0700

    refactored

--- application.js
+++ application.js
@@ -1,13 +1,43 @@
 $(document).ready(function() {
-  $('#roller button.add').on('click', function() {
-    console.log("WAT")
-    $('.dice').append('<div class="die">0</div>');
-  });
-
-  $('#roller button.roll').on('click', function() {
-    $('.die').each(function(k, die) {
-      var value = Math.floor((Math.random()*6)+1);
-      $(die).text(value);
-    });
-  });
+  $('#roller button.add').on('click', addDice);
+  $('#roller button.roll').on('click', randomNumber);
+  $('.dice').append(dieView.render().$elem);
 });
+
+function addDice(e) {
+  console.log("WAT")
+  $('.dice').append('<div class="die">0</div>');
+};
+
+
+function randomNumber(e) {
+  $('.die').each(function(k, die) {
+    var value = Math.floor((Math.random()*6)+1);
+    $(die).text(value);
+  });
+};
+
+function Die(data){
+  this.value = data.value;
+}
+
+Die.prototype.roll = function(){
+  this.value = Math.floor((Math.random()*6)+1);
+}
+
+function DieView(model){
+  this.$elem = $('<div class="die"></div>');
+  this.model = model;
+  $(this.model).on('update', this.render);
+}
+
+DieView.prototype.render = function(){
+  this.$elem.html(this.model.value);
+  return this;
+}
+
+die = new Die({value: 1});
+
+dieView = new DieView(die);
+
+console.log(die, dieView);
